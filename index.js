fc0ee6fcb7a4f4355ec7348988201f0823fa6733
Ariful129 <u1904129@student.cuet.ac.bd>
2024-05-18 00:12:24 +0600

    ..

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 // const allowedOrigin = ['https://auth-private-all.web.app/']
 // middleware
-// app.use(cors({
-  // origin: 'https://auth-private-all.web.app/',
-  // credentials: true
-// }))
 
-app.use(cors())
+const allowedOrigins = ['http://localhost:5173', 'https://skill-share-client-final1.vercel.app']; 
+app.use(cors({ origin: function (origin, callback) { 
+if (!origin) 
+  return callback(null, true); 
+if (allowedOrigins.indexOf(origin) === -1) { 
+  const msg = 'The CORS policy for this site does not allow access from the specified origin.'; 
+  return callback(new Error(msg), false); 
+} 
+return callback(null, true);
+ }}));   
+
 
 // app.use(cors({
 //   origin: 'https://skill-share-client-final1.vercel.app'
 // }));
-app.use(express.json());
+// app.use(express.json());
 
 // app.use(cors());
 // app.use(express.json());
